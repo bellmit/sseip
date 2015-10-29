47d7dfe1d8dcdb509aec0bd2349de38481d3f078
sdrkyj <sdrkyj@sina.com>
2015-10-29 23:10:10 +0800

    edit

---
+++ src/main/java/com/syzc/sseip/pasture/L.java
@@ -0,0 +1,11 @@
+package com.syzc.sseip.pasture;
+
+import org.apache.log4j.Logger;
+
+public class L {
+    private final static Logger logger = Logger.getLogger(L.class);
+
+    public static void main(String[] args) {
+        logger.trace("traced!");
+    }
+}
