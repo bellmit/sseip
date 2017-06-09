c298d0ff7cdcb4a9aee54967dd19ad6953cf9c1d
sdrkyj <sdrkyj@sina.com>
2017-06-09 18:17:07 +0800

    修改过了一点

--- src/main/java/com/syzc/sseip/entity/Group.java
+++ src/main/java/com/syzc/sseip/entity/Group.java
@@ -3,6 +3,7 @@ package com.syzc.sseip.entity;
 import java.io.Serializable;
 
 //扩展Dto，增加成员数量字段； 查询时候联合查询获取数量； 修改基本列表查询，或增加dto 列表查询
+//department
 public class Group implements Serializable {
     private static final long serialVersionUID = -5327691808977625142L;
     private Long id;
